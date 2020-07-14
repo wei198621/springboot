0fb540f130cc05f1c3679f4012e34d8580872db3
wei198621 <wei198621@126.com>
2020-07-14 21:42:59 +0800

    war 打包的两种启动文件

--- src/main/java/com/tz/leo/springboot/Application.java
+++ src/main/java/com/tz/leo/springboot/Application.java
@@ -4,46 +4,40 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 /**
  * Author: tz_wl
  * Date: 2020/7/11 13:16
- * Content:
- *
- * MapperScan             扫描dao接口所在的包
- *
+ * Content:  正常idea开发用此配置
+ * MapperScan             扫描dao接口所在的包 *
  * ComponentScan
  * EnableAutoConfiguration
  * SpringBootConfiguration
  */
 @SpringBootApplication
 @MapperScan("com.tz.leo.springboot.dao")
-public class Application {
+public class Application  {
     public static void main(String[] args) {
         System.out.println(args);
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class, args);
+    }
+}
 
-       /* LocalDateTime now= LocalDateTime.now();
-        System.out.println("开始时间是："+ now.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-        for (int i=0; i< 10000000 ; i++){
-            System.out.println("循环了第"+ (i+1));
-        }
-        LocalDateTime  end=LocalDateTime.now();
-        System.out.println("结束时间是："+ end.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-        Duration duration = Duration.between(now,end);
-        long minutes = duration.toMinutes();//相差的分钟数
-        long millis = duration.toMillis();//相差毫秒数
-        long nanos = duration.toNanos();//相差的纳秒数
-        System.out.println("************************************");
-       // System.out.println("相差的分钟数"+ minutes);
-        System.out.println("相差毫秒数"+ millis);
-       // System.out.println("相差的纳秒数"+ nanos);*/
+/*
 
+//如下配置为 war打包配置  ，暂时不用
+@SpringBootApplication
+@MapperScan("com.tz.leo.springboot.dao")
+public class Application extends SpringBootServletInitializer {
 
+    // war 打包步骤之一
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class); //指定入口类类对象即可
     }
 
+    public static void main(String[] args) {
+        System.out.println(args);
+        SpringApplication.run(Application.class, args);
+    }
 }
+*/
